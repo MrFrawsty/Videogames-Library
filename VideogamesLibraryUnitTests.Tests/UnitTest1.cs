d4e78ae8458ba6b409d667eecf622a834da4f045
MrFrawsty <kennyjoec@gmail.com>
2021-04-13 07:48:26 -0500

    Added first Unit test

--- VideogamesLibraryUnitTests.Tests/UnitTest1.cs
+++ VideogamesLibraryUnitTests.Tests/UnitTest1.cs
@@ -1,14 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
+using System.Collections.Generic;
+using VideoGames.Areas.Services;
+using VideoGames.Controllers;
+using VideoGames.Models;
 using Xunit;
 
 namespace VideogamesLibraryUnitTests.Tests
 {
-    public class UnitTest1
+    public class GamesControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Games_ShouldReturnViewWithGames()
         {
+            //Arrange
+            var mockData = new Mock<IUserData>();
+            mockData.Setup(data => data.GetGames()).Returns(GetTestGames);
+            var gamesController = new GamesController(mockData.Object);
 
+            //Act
+            var result = gamesController.Games();
+
+
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        private List<Game> GetTestGames()
+        {
+            var games = new List<Game>()
+            {new Game() { GameId = 1, Name = "Test Game 1", Genre = "Test Genre", Completed = false },
+            {new Game() { GameId = 2, Name = "Test Game 2", Genre = "Test Genre", Completed = false } } };
+
+            return games;
+                
+                
         }
     }
 }
